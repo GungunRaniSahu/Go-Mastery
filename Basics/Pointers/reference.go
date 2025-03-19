d0f7ccc62d14c75c3cd13ec8d09b423d66075f65
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-03-19 13:36:40 +0530

    Function with Pointer Parameter (Pass by Reference)

---
+++ Basics/Pointers/reference.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func updateValue(n *int) 
+{
+    *n = 100 
+}
+
+func main() 
+{
+    num := 25
+    fmt.Println("Before:", num)
+    updateValue(&num)
+    fmt.Println("After:", num)
+}
