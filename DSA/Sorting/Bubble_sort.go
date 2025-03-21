c35ef1b6de23131894186d82eee751e97a29c28b
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-03-21 22:52:38 +0530

    Bubble Sort

--- DSA/Sorting/Bubble_sort.go
+++ DSA/Sorting/Bubble_sort.go
@@ -3,39 +3,27 @@ package main
 import "fmt"
 
 
-func QuickSort(arr []int) 
-{
-	if len(arr) <= 1 {
-		return
-	}
-	
-
-	pivotIndex := partition(arr)
-	
-
-	QuickSort(arr[:pivotIndex])
-	QuickSort(arr[pivotIndex+1:])
-}
-
-
-func partition(arr []int) int {
-	pivot := arr[len(arr)-1]
-	i := 0
-	for j := 0; j < len(arr)-1; j++ {
-		if arr[j] < pivot {
-			arr[i], arr[j] = arr[j], arr[i]
-			i++
+func BubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j] 
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
 		}
 	}
-	arr[i], arr[len(arr)-1] = arr[len(arr)-1], arr[i]
-	return i
 }
 
 func main() {
 	arr := []int{64, 34, 25, 12, 22, 11, 90}
 	fmt.Println("Unsorted array:", arr)
 
-	QuickSort(arr)
+	BubbleSort(arr)
 
 	fmt.Println("Sorted array:", arr)
 }
