1e5f2a8956bc1051b16ebc3d938d55b34ca04176
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-03-22 23:28:53 +0530

    Linear Search in Go

---
+++ DSA/Searching/linear_search.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func LinearSearch(arr []int, target int) int 
+{
+    for i, num := range arr 
+  {
+        if num == target 
+    {
+            return i 
+        }
+    }
+    return -1 
+}
+
+func main() 
+{
+    arr := []int{10, 20, 30, 40, 50}
+    target := 30
+
+    index := LinearSearch(arr, target)
+
+    if index != -1 
+  {
+        fmt.Printf("Element %d found at index %d\n", target, index)
+    }
+  else 
+  {
+        fmt.Printf("Element %d not found\n", target)
+    }
+}
